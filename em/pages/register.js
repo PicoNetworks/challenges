ad88892a778d0a72d7e8c2a73de24b6c93bec226
voravor <voravor@gmail.com>
2021-03-17 00:07:10 -0400

    yeah...

--- em/pages/register.js
+++ em/pages/register.js
@@ -2,15 +2,18 @@ export default function Form() {
     const registerUser = async (event) => {
         event.preventDefault()
 
-        const res = await fetch('/api/register', {
-            body: JSON.stringify({
-                name: event.target.name.value,
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-        })
+        // TODO: this should use pipedream instead
+        // WTF is this?? why fetch? this is 2021.
+        // commenting out for now
+        // const res = await fetch('/api/register', {
+        //     body: JSON.stringify({
+        //         name: event.target.name.value,
+        //     }),
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     method: 'POST',
+        // })
 
         const result = await res.json()
         return result
